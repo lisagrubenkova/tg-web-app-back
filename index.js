e3316b228d9d5f7aecd54f0b1b60f78b0062ed25
Elizaveta <elizaveta.grubenkova@gmail.com>
2025-01-14 01:20:52 +0300

    update

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ bot.on('message', async (msg) => {
 });
 
 app.post('/web-data', async (req, res) => {
-    const {queryId, products, totalPrice} = req.body
+    const {queryId, products=[], totalPrice} = req.body
     console.log('hehe')
     try {
         await bot.answerWebAppQuery(queryId, {
