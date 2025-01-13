e8041d6a78343887b8a55b102782466a497d3bd5
Elizaveta <elizaveta.grubenkova@gmail.com>
2025-01-14 01:21:52 +0300

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bot = new TelegramBot(token, {polling: true});
 const app = express()
 
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
