4abeb310b173b8b802051a5b534b3f5de350a373
Elizaveta <elizaveta.grubenkova@gmail.com>
2025-01-14 01:51:41 +0300

    eeeee

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/web-data', async (req, res) => {
         })
         return res.status(200).json({});
     } catch (e) {
-        console.log('lol')
+        console.log(e)
         return res.status(500).json({})
     }
 })
