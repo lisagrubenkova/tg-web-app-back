d8e4b375803b2a4a8d8ab696f3cfec9b37dab81a
Elizaveta <elizaveta.grubenkova@gmail.com>
2025-01-14 02:04:29 +0300

    sddx

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ app.post('/web-data', async (req, res) => {
             id: queryId,
             title: 'Успешная покупка',
             input_message_content: {message_text: `Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice},
-            ${products.map(item => item.title).join(', ')}`}
+                }`
+            }
         })
         return res.status(200).json({});
     } catch (e) {
